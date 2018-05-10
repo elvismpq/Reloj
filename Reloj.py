2952720caa3f1327bc084467d77402a18b6a6a91
1-21-01-MD-19\ESFOT <elvis.perez01@epn.edu.ec>
2018-05-10 09:08:19 -0500

    Optimizamos el codigo

--- Reloj.py
+++ Reloj.py
@@ -1,14 +1,10 @@
 import os
 import time
-from time import sleep
-def cls():
-    os.system('cls')
-print(time.localtime())
 while True:
     t = time.localtime()
     hour = t[3]
     min = t[4]
     sec = t[5]
     print("La hora es: ",hour, ":", min, ":", sec)
-    sleep(1)
-    cls()
+    time.sleep(1)
+    os.system('cls')
